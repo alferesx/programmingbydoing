31a34213878439b0fb6a209be148653e0d9245aa
Pedro Alferes <alferesx@gmail.com>
2021-03-04 09:40:48 -0300

    Exercise 4

---
+++ NumbersandMath.py
@@ -0,0 +1,17 @@
+print("I will now count my chickens: ")
+print( " Hens " + str(25 + 30 /6))
+print("Roosters " +str(100 -25 * 3 % 4))
+print("Now i will count the eggs:")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print("Is it true that 3+2<5 - 7? ")
+print(3+2<5 - 7)
+print("What is 3 + 2?" + str(3 + 2))
+print("What is 5 - 7?" + str(5 - 7))
+print("Oh,that's why it's false ")
+print("How about some more.")
+print("Is it greater?" + str(5 > -2))
+print("Is it greater or equal?" + str(5 >= -2))
+print("Is it less or equal?" + str(5 <= -2))
+
+
+
