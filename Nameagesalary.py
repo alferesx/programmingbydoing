a79829508092c789d8d6f0a75778b037263e9a4c
Pedro Alferes <alferesx@gmail.com>
2021-03-05 12:23:54 -0300

    Exercise 11

---
+++ Nameagesalary.py
@@ -0,0 +1,6 @@
+name = input("Hello. What is your name ?: ")
+age = int(input("Hi " + name + " ! " + " How old are you ?: "))
+print("So you're " + str(age) + " ,eh? That's nott old at all!")
+money = float(input("How  much do you make, Dennis?: "))
+print(str(money) + " I hope that's per hour and not per year! LOL! ")
+
