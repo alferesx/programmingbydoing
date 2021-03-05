f6d3713f864517ca80f6d964afcfe7a0823c7656
Pedro Alferes <alferesx@gmail.com>
2021-03-05 09:34:52 -0300

    Exercise 8

---
+++ YourSchedule.py
@@ -0,0 +1,26 @@
+course1 = "English III"
+course2 = "Precalculus"
+course3 = "Music Theory"
+course4 = "Biotechnology"
+course5 = "Principles of Technology I"
+course6 = "Latin II"
+course7 = "Ap US History"
+course8 = "Business Computer Information Systems"
+teacher1 = "Ms.Lapan"
+teacher2 = "Mrs.Gideon"
+teacher3 = "Mr.David"
+teacher4 = "Ms.Palmer"
+teacher5 = "Ms.Garcia"
+teacher6 = "Mrs.Barnett"
+teacher7 = "Ms.Johannessen"
+teacher8 = "Mr.James"
+print("+----------------------------------------------------------+")
+print("| 1 |" + course1 +"|" + "   " + teacher1)
+print("| 2 |" + course2 +"|" + "   " + teacher2)
+print("| 3 |" + course3 +"|" + "   " + teacher3)
+print("| 4 |" + course4 +"|" + "   " + teacher4)
+print("| 5 |" + course5 +"|" + "   " + teacher5)
+print("| 6 |" + course6 +"|" + "   " + teacher6)
+print("| 7 |" + course7 +"|" + "   " + teacher7)
+print("| 8 |" + course8 +"|" + "   " + teacher8)
+print("+----------------------------------------------------------+")
