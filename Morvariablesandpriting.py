1693eff981c2a4d338f9c9adb3d216f534e4b1ce
Pedro Alferes <alferesx@gmail.com>
2021-03-05 09:00:27 -0300

    Exercise 5

---
+++ Morvariablesandpriting.py
@@ -0,0 +1,15 @@
+myName = "Zed A. Shaw"
+myAge = 40
+myHeight = 74
+myWeight = 180
+myEyes = "Blue"
+myTeeth = "White"
+myHair = "Brown"
+
+print("Let's talk about " + myName)
+print("He's" + str(myHeight) + "inches tall")
+print("He's" + str(myWeight) + "pounds heavy")
+print("Actually, that's not too heavy")
+print("He's got " + myEyes + "eyes and " + myHair + "hair")
+print("His teeth are usually " + myTeeth + "depending on the coffee")
+print("If i add " + str(myAge) + " , " + str(myHeight) + " , and " + str(myWeight) + " I get " + (str(myAge) + " " + " " + str(myHeight)+ " " + str(myWeight)) + ".")
