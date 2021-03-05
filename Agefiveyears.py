625ddf2188b88e3c17acc9d4107c9cb1a2febdf7
Pedro Alferes <alferesx@gmail.com>
2021-03-05 12:49:59 -0300

    Exercise 12

---
+++ Agefiveyears.py
@@ -0,0 +1,4 @@
+name = input("Hello. What is your name ?: ")
+age = int(input("Hi," + name + " How old are you?: "))
+print("Did you know that in five years you will be " + str(age + 5) + " old? ")
+print("And five years ago you were  " + str(age - 5) + " imagine that! ")
